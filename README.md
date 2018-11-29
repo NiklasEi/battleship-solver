16875952738529dccfdf176b11d73e840dc01a83
Niklas Eicker <nikle1@gmx.de>
2018-11-29 18:01:09 +0100

    add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Battleship solver
+
+WIP! (easy puzzles can be solved already)
+
+This is a solver for [battleship-like](https://en.wikipedia.org/wiki/Battleship_%28puzzle%29) logic puzzles.
+
+The puzzle has to be insert into [solve.py](solve.py), then simply execute the script and watch the progress in the console.
--- solve.py
+++ solve.py
@@ -4,7 +4,7 @@ from solver.solver import BattleShipSolver
 ships = {2: 2, 5: 1}
 grid_columns = 5  # number of columns in the grid
 grid_rows = grid_columns  # number of rows in the grid
-counts_columns = [1, 3, 1, 3, 1]  # counts from left to right
-counts_rows = [2, 2, 0, 0, 5]  # counts from top to bottom
+counts_columns = [1, 3, 1, 3, 1]  # number of ship elements from left to right
+counts_rows = [2, 2, 0, 0, 5]  # number of ship elements from top to bottom
 
 BattleShipSolver(grid_columns, grid_rows, ships, counts_columns, counts_rows).start()
