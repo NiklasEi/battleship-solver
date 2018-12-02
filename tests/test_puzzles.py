f4950483907e8e4cb281d968b421e0031e62f383
Niklas Eicker <nikle1@gmx.de>
2018-12-02 01:41:10 +0100

    parse puzzles from json file

--- tests/test_puzzles.py
+++ tests/test_puzzles.py
@@ -1,27 +1,39 @@
 import os
 import unittest
 import json
+from solver.solver import BattleShipSolver
+from typing import Dict
 from unittest import TestCase
 
 
 class TestPuzzles(TestCase):
+    """
+    Test the solver on a collection of puzzles
+    """
     def setUp(self):
+        """
+        Load the json file with the test puzzles
+        :return:
+        """
         with open("resources" + os.sep + "test_puzzles.json") as json_data:
             self.puzzle_json = json.load(json_data)
+            print("loaded " + str(self.puzzle_json))
 
     def test_puzzles(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        for puzzle_json_dict in self.puzzle_json["puzzles"]:
+            puzzle = self.load_puzzle_data(puzzle_json_dict)
+            BattleShipSolver(puzzle["columns"], puzzle["rows"], puzzle["ships"], puzzle["counts_columns"], puzzle["counts_rows"]).start()
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    @staticmethod
+    def load_puzzle_data(puzzle_dict) -> Dict:
+        """
+        Prepare the dictionary from json data for direct use in the solver
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split('2')
+        :param puzzle_dict: dictionary loaded from the json file
+        :return: dictionary with correctly formatted values for use in the solver
+        """
+        puzzle_dict["ships"] = {int(key):val for key, val in puzzle_dict["ships"].items()}
+        return puzzle_dict
 
 
 if __name__ == '__main__':
